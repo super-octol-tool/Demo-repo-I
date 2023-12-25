7eb13bcc7a523637f1c0bbf704486796eb3fd8ae
super-octol-tool <henwilsfsit@gmail.com>
2023-12-25 03:31:46 +0000

    Inserted the bash script

---
+++ shebang/bash.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+
+name "Patricia"
+
+
+echo "Good morning $name!!"
+
+sleep 1
+
+echo "you are looking good $name!!"
+
+sleep 1
+
+echo "you have the most pretty face this morning >
